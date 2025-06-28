a522d1a0e2c17148087a5c3d4831519c13f8a8a5
Ekam Singh <ekamsinghh24@gmail.com>
2025-06-28 21:14:10 +0530

    Added upload image api

---
+++ Backend/src/middlewares/uploadMiddleware.js
@@ -0,0 +1,29 @@
+const multer = require('multer');
+
+//Configuring Storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+//File Filter
+const fileFilter = (req,file,cb) => {
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+    if(allowedTypes.includes(file.mimetype)){
+        cb(null,true);
+    }
+    else{
+        cb(new Error("Only .jpeg, .jpg, .png formats are allowed"),false);
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter
+});
+
+module.exports = upload;
--- Backend/src/routes/v1/authRoutes.js
+++ Backend/src/routes/v1/authRoutes.js
@@ -1,10 +1,28 @@
 const express= require('express');
 const { registerUser, loginUser, getUserProfile }= require('../../controllers/authController');
 const { protect } = require('../../middlewares/authMiddleware');
+const upload = require('../../middlewares/uploadMiddleware');
 const router=express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 router.get('/profile',protect,getUserProfile);
 
+router.post('/upload-image',upload.single('image'), (req, res) => {
+    if(!req.file){
+        return res.status(400).json({
+            message:"No file Uploaded",
+            success:false,
+            data:{},
+            error:"Bad Request"
+        });
+    }
+    //creating image url
+    const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+    // req.protocol is http or https and req.get('host') is the host name of the server
+    res.status(200).json({ 
+        imageUrl 
+    });
+
+})// upload.single() is a multer middleware
 module.exports = router;
